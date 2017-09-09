eb9a79ca5d51c4b458c5d7f6c37347f379970370
DESKTOP-0VAEMFL\joaom <1160907@isep.ipp.pt>
2017-09-09 20:35:50 +0100

    Added main activity buttons and listeners

--- YourOwnSoundBoard/app/src/main/java/freitzzz/yourownsoundboard/MainActivity.java
+++ YourOwnSoundBoard/app/src/main/java/freitzzz/yourownsoundboard/MainActivity.java
@@ -1,13 +1,48 @@
 package freitzzz.yourownsoundboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    /**
+     * Touch buttons Make,Load and About
+     */
+    private Button about;
+    private Button load;
+    private Button make;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createButtons();
+    }
+    private void createButtons(){
+        make=(Button)findViewById(R.id.make);
+        load=(Button)findViewById(R.id.load);
+        about=(Button)findViewById(R.id.about);
+        createListeners();
+    }
+    private void createListeners(){
+        make.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open maker
+            }
+        });
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open loader
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,About.class));
+            }
+        });
     }
 }
